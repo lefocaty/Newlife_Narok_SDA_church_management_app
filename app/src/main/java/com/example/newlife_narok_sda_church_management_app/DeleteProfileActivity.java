c4bf9a37209cf1860fe7a765d52af1ca88d7d7cd
Lefocaty <lefocatynyangweso@gmail.com>
2022-08-10 20:00:15 +0300

    Delete Profile Activity

---
+++ app/src/main/java/com/example/newlife_narok_sda_church_management_app/DeleteProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.newlife_narok_sda_church_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_profile);
+    }
+}
